fea81f47a7beb462da53f1db6cffc59ce1e77c8c
rjc0des <rohithjayaraj90@gmail.com>
2026-01-29 12:10:24 +0530

    feat: added help

--- cmd/run.go
+++ cmd/run.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"text/tabwriter"
 )
 
 func Run() {
@@ -29,7 +30,27 @@ func Run() {
 		DeleteCommand(args[2])
 	case "mark-todo", "mark-in-progress", "mark-done":
 		StatusUpdate(args[1:])
+	case "help":
+		printHelp()
 	default:
-		fmt.Println("Command not found")
+		printHelp()
 	}
+}
+
+func printHelp() {
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 2, ' ', 0)
+
+	fmt.Println("Tasker Cli")
+	fmt.Print("An cli app for todo task without internet or database\n\n")
+	fmt.Fprintln(w, "Command\tUsage\tdescription")
+	fmt.Fprintln(w, "-------\t-----\t-----------")
+	fmt.Fprintln(w, "add\tadd <task description>\tAdd task")
+	fmt.Fprintln(w, "list\tlist [todo|in-progress|done]\tList the task")
+	fmt.Fprintln(w, "update\tupdate <task id> <task description>\tUpdate the task description")
+	fmt.Fprintln(w, "delete\tdelete <task id>\tDelete a task using task id")
+	fmt.Fprintln(w, "mark-todo\tmark-todo <task id>\tUpdate the task status to todo")
+	fmt.Fprintln(w, "mark-in-progress\tmark-in-progress <task id>\tUpdate the task status to in-progress")
+	fmt.Fprintln(w, "mark-done\tmark-done <task id>\tUpdate the task status to done")
+
+	w.Flush()
 }
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"task-cli/cmd"
 )
 
 func main() {
-	fmt.Println("Task cli")
 	cmd.Run()
 }
